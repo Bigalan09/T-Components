b0811e21f7ea774793dff6087aaf79f45afeef9e
Alan Gardner <bigalan09@gmail.com>
2022-05-31 13:22:45 +0000

    Remove collapable menu

--- t-components/src/components/t-sidebar/t-sidebar.tsx
+++ t-components/src/components/t-sidebar/t-sidebar.tsx
@@ -7,22 +7,13 @@ import { Component, Host, h, State } from '@stencil/core';
 })
 export class TSidebar {
 
-  @State()
-  private open:boolean = true;
-
-  private onClick() {
-    this.open = !this.open;
-  }
-  
   render() {
     return (
-      <Host
-        aria-hidden={this.open ? 'false' : 'true'}
-        style={{
-          'width': this.open ? "13vw" : "auto"
-        }}>
-        <h1 onClick={() => this.onClick()}>{this.open ? "T-Components" : ">" }</h1>
-        {this.open ? <slot></slot> : <strong></strong> }
+      <Host>
+        <aside>
+        <h1>T-Components</h1>
+          <slot></slot>
+        </aside>
       </Host>
     );
   }
